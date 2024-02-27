48a0ce6aec7695729a94ff41d2bd30b38f82317a
a-iv <alexander.vl.ivanov@gmail.com>
2024-02-28 00:33:48 +0500

    Remote control implemented.

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(4)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(6)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(2)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(5)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    radio.sendNumber(0)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    radio.sendNumber(9)
 })
